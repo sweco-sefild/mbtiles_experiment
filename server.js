fbe9435366a3939519ca34524a68e9122a5b5dac
FHilding <tony.fat@gmail.com>
2018-04-22 19:52:23 +0200

    Restructured server

--- server.js
+++ server.js
@@ -8,59 +8,67 @@ var serveStatic = require('serve-static');
 var path = require('path');
 var exec = require('child_process').exec;
 var result = '';
+var currentSource = '';
 
-tilelive.load('mbtiles:///home/ubuntu/data/se_500k.mbtiles', function (err, source) {
-    if (err) {
-        throw err;
-    }
-    app.set('port', 7777);
-    app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
+function loadData() {
+    tilelive.load('mbtiles:///home/ubuntu/data/se_500k.mbtiles', function (err, source) {
+        if (err) {
+            throw err;
+        }
+        currentSource = source
+    });
+}
+
+
+app.set('port', 7777);
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get(/^\/v2\/tiles\/(\d+)\/(\d+)\/(\d+).pbf$/, function (req, res) {
+
+    var z = req.params[0];
+    var x = req.params[1];
+    var y = req.params[2];
+
+    console.log('get tile %d, %d, %d', z, x, y);
+
+    currentSource.getTile(z, x, y, function (err, tile, headers) {
+        if (err) {
+            res.status(404)
+            res.send(err.message);
+            console.log(err.message);
+        } else {
+            res.set(headers);
+            res.send(tile);
+        }
+    });
+});
+
+app.get('/json', function (req, res) {
+    res.sendFile(path.join(__dirname + '/minimal.json'));
+});
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/minimal.html'));
+});
+app.get('/data', function (req, res) {
+    var child = exec('sh run.sh');
+
+    child.stdout.on('data', function (data) {
+        result += data;
     });
 
-    app.get(/^\/v2\/tiles\/(\d+)\/(\d+)\/(\d+).pbf$/, function (req, res) {
-
-        var z = req.params[0];
-        var x = req.params[1];
-        var y = req.params[2];
-
-        console.log('get tile %d, %d, %d', z, x, y);
-
-        source.getTile(z, x, y, function (err, tile, headers) {
-            if (err) {
-                res.status(404)
-                res.send(err.message);
-                console.log(err.message);
-            } else {
-                res.set(headers);
-                res.send(tile);
-            }
-        });
+    child.on('close', function () {
+        console.log('done', result);
+        loadData()
+        res.send("Data fixed")
     });
+});
 
-    http.createServer(app).listen(app.get('port'), function () {
-        console.log('Express server listening on port ' + app.get('port'));
-    });
-    app.get('/json', function (req, res) {
-        res.sendFile(path.join(__dirname + '/minimal.json'));
-    });
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname + '/minimal.html'));
-    });
-    app.get('/data', function (req, res) {
-        var child = exec('sh run.sh');
-
-        child.stdout.on('data', function (data) {
-            result += data;
-        });
-
-        child.on('close', function () {
-            console.log('done', result);
-            res.send("Data fixed")
-        });
-    });
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
 
